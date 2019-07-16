de207910fe83c641176b3ad8b2df5196a66b926a
Graham Fairweather <xotic750@gmail.com>
2019-07-16 22:47:02 +0200

    Terraformed
    Auto build

--- __tests__/object-assign-x.test.js
+++ __tests__/object-assign-x.test.js
@@ -1,28 +1,5 @@
 let assign;
 
-if (typeof module === 'object' && module.exports) {
-  require('es5-shim');
-  require('es5-shim/es5-sham');
-
-  if (typeof JSON === 'undefined') {
-    JSON = {};
-  }
-
-  require('json3').runInContext(null, JSON);
-  require('es6-shim');
-  const es7 = require('es7-shim');
-  Object.keys(es7).forEach(function(key) {
-    const obj = es7[key];
-
-    if (typeof obj.shim === 'function') {
-      obj.shim();
-    }
-  });
-  assign = require('../../index.js');
-} else {
-  assign = returnExports;
-}
-
 let supportsGetSet;
 try {
   Object.defineProperty({}, 'a', {
