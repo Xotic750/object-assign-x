c8ca28b0046c2f2906101b5ecd65ff3215d1e8c2
Graham Fairweather <xotic750@gmail.com>
2019-08-07 03:55:17 +0200

    Auto build

--- dist/object-assign-x.js
+++ dist/object-assign-x.js
@@ -2,7 +2,7 @@
 {
   "author": "Graham Fairweather",
   "copywrite": "Copyright (c) 2017",
-  "date": "2019-08-06T21:42:35.602Z",
+  "date": "2019-08-07T01:55:10.866Z",
   "describe": "",
   "description": "Used to copy the values of all enumerable own properties from one or more source objects to a target object.",
   "file": "object-assign-x.js",
