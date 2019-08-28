6b5c2653c41d52d335a5452abe1184241b1e14ee
Graham Fairweather <xotic750@gmail.com>
2019-08-28 18:00:21 +0200

    Maintenance

--- dist/object-assign-x.js
+++ dist/object-assign-x.js
@@ -2,7 +2,7 @@
 {
   "author": "Graham Fairweather",
   "copywrite": "Copyright (c) 2017",
-  "date": "2019-08-27T21:14:14.862Z",
+  "date": "2019-08-28T16:00:13.626Z",
   "describe": "",
   "description": "Used to copy the values of all enumerable own properties from one or more source objects to a target object.",
   "file": "object-assign-x.js",
